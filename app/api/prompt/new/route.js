8de661022898a5c92b9555a664300556e0b5bb1e
fpsales2003 <95240671+fpsales2003@users.noreply.github.com>
2023-07-25 16:26:24 -0400

    first commit

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,27 @@
+// API route
+// Lambda function. Every time its called it connects to DB then disconnects when finished
+
+import { connectToDB } from '@utils/database';
+import Prompt from '@models/prompt';
+
+export const POST = async(req, res) => {
+    // extract data from post request
+    const { userId, prompt, tag } = await req.json();
+
+    try {
+        await connectToDB();
+        const newPrompt = new Prompt({
+            creator:userId,
+            tag,
+            prompt
+        })
+
+        await newPrompt.save();
+
+        // status 201 = successful post request
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
+    } catch (error) {
+        // status 500 = server error
+        return new Response('Failed to create a new prompt',  { status: 500 })
+    }
+}
